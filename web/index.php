360125523e57b42787609af48bd6fa27e66e9c71
Mahmut Kavici <mkavici@gmail.com>
2021-05-03 05:01:18 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,27 @@
 <?php
 
-// Do nothing. Stub file to make VestaCP Plugin Manager link to us.
+// Tab name
+$TAB = ".net Web Apps";
+
+// Include vesta functions
+include($_SERVER['DOCUMENT_ROOT'] . "/inc/main.php");
+
+if (isset($_POST['action']) && $_POST['action'] == "install"
+    && isset($_POST['app']) && !empty($_POST['app'])
+    && isset($_POST['web_domain']) && !empty($_POST['web_domain'])
+) {
+    $app = trim($_POST['app']);
+    $web_parts = explode("|", $_POST['web_domain']);
+    $user_name = trim($web_parts[0]);
+    $web_domain = trim($web_parts[1]);
+
+    if ($user == 'admin' || $user == $user_name) {
+        $output = Vesta::exec('v-install-dotnet-app', $app, $user_name, $web_domain);
+    } else {
+        $output = __("You are not allowed to perform this action");
+    }
+
+    Vesta::render_cmd_output($output, __("Installing") . " $app", $_SERVER['REQUEST_URI']);
+} else {
+    Vesta::render("/templates/install.php", ['plugin' => 'web-apps', 'data' => $data]);
+}
